f2f58a28e0485567d58c3ee9ba2305213859097d
Smokeybro916 <kyleluangasa@gmail.com>
2023-09-26 19:54:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// WRITE YOUR CODE IN HERE:
+
+const list = document.querySelectorAll('li')
+for (let li of list){
+    li.classList.toggle('highlight');
+}
+
+/* In here I was instructed to use the classList property to get the class 'highlight' with toggle */
